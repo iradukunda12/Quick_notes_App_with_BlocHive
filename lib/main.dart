32abce40fc611e3e866418f1acc89e8b7743b06a
Iradukunda12 <iradukundakvn8@gmail.com>
2025-05-06 12:48:33 +0200

    chores: configured the supbase keys and api and initialize in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quick_notes_app/screens/home.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  //Setting up the supabase
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env["Supabase_Project_URL"] ?? 'Missing URL',
+    anonKey: dotenv.env["Supabase_Project_API_KEY"] ?? 'missing API Key',
+  );
   runApp(const MyApp());
 }
 
